da92e2da81413535425b41d0d99242a03e6f3584
bo-nicolaisen <bo@squirrelworks.dk>
2024-04-29 21:37:09 +0200

    base code

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,18 @@
 // write cool JS hwere!!
+
+const baseColors = [
+    'aqua', 
+    'fuchsia',
+    'lime',
+    'yellow',
+    'cyan',
+    'magenta',
+    'orange',
+    'pink',
+    'red',
+    'blue'
+  ];
+
+
+
+  
